22d2338bcd8006b952eb6c0dd1005106eab7d868
harinik05 <hka747910@gmail.com>
2023-08-29 11:38:06 -0400

    summarize text and generate summary prompt

--- openai model/summarize_doc.py
+++ openai model/summarize_doc.py
@@ -17,6 +17,10 @@ reset the summary
 def clear_summary():
     st.session_state['summary'] = ""
 
+'''
+get_prompt will take the text that is currently present and provide with one of the following
+options
+'''
 def get_prompt():
     text = st.session_state['text']
     if text is None or text == '':
@@ -31,7 +35,9 @@ def get_prompt():
     return prompt
 
 try:
-    # Set page layout to wide screen and menu item
+    '''
+    set the page layout
+    '''
     menu_items = {
     'Get help': None,
     'Report a bug': None,
@@ -44,14 +50,19 @@ try:
 
     llm_helper = LLMHelper()
 
+    '''
+    summarization 
+    '''
     st.markdown("## Summarization")
-    # radio buttons for summary type
     summary_type = st.radio(
         "Select a type of summarization",
         ["Basic Summary", "Bullet Points", "Explain it to a second grader"],
         key="visibility"
     )
-    # text area for user to input text
+    
+    '''
+    prompt the user to enter some text that they want summarized
+    '''
     st.session_state['text'] = st.text_area(label="Enter some text to summarize",value='A neutron star is the collapsed core of a massive supergiant star, which had a total mass of between 10 and 25 solar masses, possibly more if the star was especially metal-rich.[1] Neutron stars are the smallest and densest stellar objects, excluding black holes and hypothetical white holes, quark stars, and strange stars.[2] Neutron stars have a radius on the order of 10 kilometres (6.2 mi) and a mass of about 1.4 solar masses.[3] They result from the supernova explosion of a massive star, combined with gravitational collapse, that compresses the core past white dwarf star density to that of atomic nuclei.', height=200)
     st.button(label="Summarize", on_click=summarize)
 
@@ -60,11 +71,13 @@ try:
     if 'summary' in st.session_state:
         summary = st.session_state['summary']
 
-    # displaying the summary
+    '''
+    this is used to summarize the result and prompt
+    '''
     st.text_area(label="Summary result", value=summary, height=200)
     st.button(label="Clear summary", on_click=clear_summary)
 
-    # displaying the prompt that was used to generate the summary
+    
     st.text_area(label="Prompt",value=get_prompt(), height=400)
     st.button(label="Summarize with updated prompt")
 
