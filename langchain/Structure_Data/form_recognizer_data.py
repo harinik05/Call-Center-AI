5b93866054ef6cf194e73cd16d7447e24dad121c
harinik05 <hka747910@gmail.com>
2023-08-29 21:10:35 -0400

    constructor params established

--- langchain/Structure_Data/form_recognizer_data.py
+++ langchain/Structure_Data/form_recognizer_data.py
@@ -6,17 +6,30 @@ from dotenv import load_dotenv
 class AzureFormRecognizerClient:
     '''
     defines the constructor for the function
+    @input: form recognizer endpoint and form recognizer key
     '''
     def __init__(self, form_recognizer_endpoint: str = None, form_recognizer_key: str = None):
-
+        '''
+        initialize the env variables from the .env file if it exists
+        '''
         load_dotenv()
-
+        
+        '''
+        initializes several attributes of the class using the values provided as
+        parameters or from env variables
+        1. pages per embeddings: attribute is set to an integer value obtained from 
+        the env variable. If its not defined, then default is 2
+        2. session to exclude: initialized as a list of strings containing section nams to 
+        exclude. appears to be hard coded and contains sections lkke footnote, etc...
+        3. form recognizer endpoint: this attribute is set to the value provided 
+        4. key param also established through 
+        '''
         self.pages_per_embeddings = int(os.getenv('PAGES_PER_EMBEDDINGS', 2))
         self.section_to_exclude = ['footnote', 'pageHeader', 'pageFooter', 'pageNumber']
 
         self.form_recognizer_endpoint : str = form_recognizer_endpoint if form_recognizer_endpoint else os.getenv('FORM_RECOGNIZER_ENDPOINT')
         self.form_recognizer_key : str = form_recognizer_key if form_recognizer_key else os.getenv('FORM_RECOGNIZER_KEY')
-
+    
     def analyze_read(self, formUrl):
 
         document_analysis_client = DocumentAnalysisClient(
